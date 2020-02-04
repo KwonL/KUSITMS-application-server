7c25362f8cd3f0c48e7163a9afe75971c348455d
KwonL <lkh116@snu.ac.kr>
2020-02-05 00:09:51 +0900

    hotfix

--- server/apps/apply/admin.py
+++ server/apps/apply/admin.py
@@ -1,8 +1,13 @@
 from django.contrib.admin.decorators import register
 from django.contrib.admin import ModelAdmin
-from apps.apply.models import ApplyForm
+from apps.apply.models import ApplyForm, SNSImage
 
 
 @register(ApplyForm)
 class ApplyFormAdmin(ModelAdmin):
+    pass
+
+
+@register(SNSImage)
+class SNSImageAdmin(ModelAdmin):
     pass

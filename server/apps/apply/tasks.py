a7a05742e60170f67aaff8c5002e04f40a1d4751
KwonL <lkh116@snu.ac.kr>
2020-07-04 01:20:39 +0900

    Print mailing exception

--- server/apps/apply/tasks.py
+++ server/apps/apply/tasks.py
@@ -1,4 +1,5 @@
 import os
+from traceback import print_exc
 
 from django.conf import settings
 from django.core.mail import send_mail
@@ -7,9 +8,12 @@ from huey.contrib.djhuey import task
 
 @task(retries=2, retry_delay=60)
 def send_new_apply_notification(name, university, team):
-    send_mail(
-        f"새로운 지원자 등록: {team} - {university} - {name}",
-        "제곧내",
-        from_email=settings.EMAIL_HOST_USER,
-        recipient_list=os.getenv("NOTIFICATION_MAIL_TARGETS").split(","),
-    )
+    try:
+        send_mail(
+            f"새로운 지원자 등록: {team} - {university} - {name}",
+            "제곧내",
+            from_email=settings.EMAIL_HOST_USER,
+            recipient_list=os.getenv("NOTIFICATION_MAIL_TARGETS").split(","),
+        )
+    except Exception:
+        print_exc()

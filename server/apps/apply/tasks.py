80804d8212a997f1ec9674db7cb25d2582690c5e
KwonL <lkh116@snu.ac.kr>
2020-07-04 00:06:19 +0900

    Add max retries for task

--- server/apps/apply/tasks.py
+++ server/apps/apply/tasks.py
@@ -5,7 +5,7 @@ from django.core.mail import send_mail
 from huey.contrib.djhuey import task
 
 
-@task()
+@task(retries=2, retry_delay=60)
 def send_new_apply_notification(name, university, team):
     send_mail(
         f"새로운 지원자 등록: {team} - {university} - {name}",
